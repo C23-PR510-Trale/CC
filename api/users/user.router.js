d2b27fbc5fe151eea4d4c99c431d301369cc6b1e
evaniatrafika <68263325+evaniatrafika@users.noreply.github.com>
2023-06-15 01:00:00 +0700

    Update user.router.js

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -7,12 +7,20 @@ const {
   getUsers,
   updateUser,
   deleteUser,
+  getData,
+  getDataByTripId,
+  getVolunByTripId,
+  getVolun,
 } = require("./user.controller");
 router.get("/",getUsers);
-router.post("/register",createUser);
+router.post("/register", createUser);
 router.get("/:id",checkToken, getUserByUserId);
 router.post("/login", login);
 router.patch("/update",checkToken, updateUser);
 router.delete("/",checkToken, deleteUser);
+router.get("/trip",getData);
+router.get("/trip/:id", getDataByTripId);
+router.get("/volun",getVolun);
+router.get("/volun/:id", getVolunByTripId);
 
 module.exports = router;
